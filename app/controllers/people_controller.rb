738c627ccdf3511d59c5e5f5b368d7c0029c06c0
Mario Adamo Jr <marioadamojr@gmail.com>
2019-08-26 22:18:31 +0000

    Automatically backed up by Learn

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,22 @@
+class PeopleController < ApplicationController
+  def new
+    @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
+  end
+
+  def create
+    Person.create(person_params)
+    redirect_to people_path
+  end
+
+  def index
+    @people = Person.all
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name)
+  end
+end
